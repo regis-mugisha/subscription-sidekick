68ec9a614b8f1ee1a7edca2bc8dad546ab375985
regis-mugisha <mugisharegis72@gmail.com>
2025-10-12 12:57:52 +0200

    feat: add timestamps

--- db/schema.ts
+++ db/schema.ts
@@ -3,6 +3,7 @@ import {
   numeric,
   pgEnum,
   pgTable,
+  timestamp,
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
@@ -15,20 +16,22 @@ export const billingCycleEnum = pgEnum("billing_cycles", [
   "custom",
 ]);
 
-export const statusEnum = pgEnum("status", [
+export const subscriptionStatusEnum = pgEnum("subscription_status", [
   "active",
   "trial",
   "paused",
   "canceled",
 ]);
 
-export const subscription = pgTable("subscriptions", {
+export const subscriptions = pgTable("subscriptions", {
   id: uuid("id").defaultRandom().primaryKey(),
   userId: varchar("user_id").notNull(), //clerk user id
   service: varchar("service").notNull(),
   plan: varchar("plan").notNull(),
   amount: numeric("amount_usd", { precision: 10, scale: 2 }).notNull(),
   billingCycle: billingCycleEnum("billing_cycle").default("monthly"),
-  status: statusEnum("status").default("active"),
-  renewalDate: date("next_renewal").notNull(),
+  status: subscriptionStatusEnum("status").default("active"),
+  renewalDate: date("renewal_date").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
