79291b3dc43db84dce3d1cbcde00bd247d97f99d
regis-mugisha <mugisharegis72@gmail.com>
2025-10-12 12:48:58 +0200

    refactor: move migrations dir to ./db and add (verbose,strict) rules

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,11 @@ config({ path: ".env" });
 
 export default defineConfig({
   schema: "./db/schema.ts",
-  out: "./migrations",
+  out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  verbose: true,
+  strict: true,
 });
